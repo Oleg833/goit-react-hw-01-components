ade513d1c23b3305f2984f8751ef86aed19249a9
Oleg833 <oleg.kovtun447@gmail.com>
2023-04-10 23:15:19 +0300

    style add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,12 @@ import BoxTitle from "components/BoxTitle/BoxTitle";
 import user from 'assets/users.json'
 import Profile from 'components/Profile/Profile'
 import Statistics from "./Statistics/Statistics";
-import data from 'assets/data.json'
+import data from 'assets/data.json';
+import friends from 'assets/friends.json';
+import FriendsList from "./FriendsList/FriendsList";
+import transactions from 'assets/transactions.json'
+import TransactionsHistory from "./TransactionsHistory/TransactionsHistory";
+
 
 export const App = () => {
   return (
@@ -18,8 +23,9 @@ export const App = () => {
     <BoxTitle title="Statistics" />
       <Statistics title="Upload stats" data={data} />
       <BoxTitle title="Friends list" />
+      <FriendsList friends={friends} />
       <BoxTitle title="Transactions history" />
-      
+      <TransactionsHistory items={transactions} />
   </div>
   );
 };
---
+++ src/components/FriendsList/FriendsList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import FriendsListItem from "./FriendsListItem";
+import css from './FriendsList.module.css';
+
+const FriendsList = ({friends}) => {
+    return (
+      <ul className={css.friendslist}>
+        {friends.map(friend => {
+        return <FriendsListItem key={friend.id} {...friend} />;
+        })}
+      </ul>
+    );
+};
+
+FriendsList.propTypes = {
+friends: PropTypes.arrayOf(
+PropTypes.shape({
+avatar: PropTypes.string.isRequired,
+name: PropTypes.string.isRequired,
+isOnline: PropTypes.bool.isRequired,
+id: PropTypes.number.isRequired,
+})
+),
+};
+
+export default FriendsList;
